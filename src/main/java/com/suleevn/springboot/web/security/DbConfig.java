401b1b2950fd0ef697ea8002a20eb42ac602a317
Sirenitix <suleevn@gmail.com>
2022-06-01 23:52:06 +0600

    Update DbConfig.java

--- src/main/java/com/suleevn/springboot/web/security/DbConfig.java
+++ src/main/java/com/suleevn/springboot/web/security/DbConfig.java
@@ -20,9 +20,9 @@ public class DbConfig  {
     public DriverManagerDataSource dataSource() {
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
         driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/clients");
-        driverManagerDataSource.setUsername("admin");
-        driverManagerDataSource.setPassword("12345678");
+        driverManagerDataSource.setUrl("jdbc:mysql://ilzyz0heng1bygi8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/bscqtti68if8ojdc");
+        driverManagerDataSource.setUsername("f0gjoldy6vl4u5pb");
+        driverManagerDataSource.setPassword("n8rti1hje5tdnnzw");
         return driverManagerDataSource;
     }
 }
